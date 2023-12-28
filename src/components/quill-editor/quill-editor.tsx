a229e0224324ba765fc2a3c79e0d7aab908ff237
romuloromulo <neoromulo@gmail.com>
2023-12-28 17:22:07 -0300

    implementando barra de edição usando Qill

--- src/components/quill-editor/quill-editor.tsx
+++ src/components/quill-editor/quill-editor.tsx
@@ -2,12 +2,32 @@
 import { useAppState } from "@/lib/providers/state-providers";
 import { File, Folder, workspace } from "@/lib/supabase/supabase.types";
 import React, { useCallback, useState } from "react";
+import "quill/dist/quill.snow.css";
 
 interface QuillEditorProps {
   dirType: "workspace" | "folder" | "file";
   fileId: string;
   dirDetails: File | Folder | workspace;
 }
+var TOOLBAR_OPTIONS = [
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
 
 const QuillEditor: React.FC<QuillEditorProps> = ({
   dirType,
@@ -17,18 +37,33 @@ const QuillEditor: React.FC<QuillEditorProps> = ({
   const { state, workspaceId, folderId, dispatch } = useAppState();
   const [quill, setQuill] = useState<any>(null);
 
-  const wrapperRef = useCallback((wrapper) => {
+  const wrapperRef = useCallback(async (wrapper: any) => {
     if (typeof window !== "undefined") {
       if (wrapper === null) return;
-      wrapper.innerHTML ='';
-      const editor = document.createElement('div');
-      wrapper.append(Editor)
+      wrapper.innerHTML = "";
+      const editor = document.createElement("div");
+      wrapper.append(editor);
+      const Quill = (await import("quill")).default;
+      // const QuillCursors = (await import("quill-cursors")).default;
+      // Quill.register("modules/cursors", QuillCursors);
+      const q = new Quill(editor, {
+        theme: "snow",
+        modules: {
+          toolbar: TOOLBAR_OPTIONS,
+          // cursors: {
+          //   transformOnTextChange: true,
+          // },
+        },
+      });
+      setQuill(q);
     }
   }, []);
 
   return (
     <>
+      {/* <div className="flex justify-center items-center flex-col mt-2 relative"> */}
       <div id="container" ref={wrapperRef} className="max-w-[800]"></div>
+      {/* </div> */}
     </>
   );
 };
