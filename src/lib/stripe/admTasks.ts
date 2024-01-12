25acbc4c25a11814125c6426d2e61cb8af7a8bdd
romuloromulo <neoromulo@gmail.com>
2024-01-12 14:38:04 -0300

    implementando copyBillingDetailsToCostumer

--- src/lib/stripe/admTasks.ts
+++ src/lib/stripe/admTasks.ts
@@ -1,7 +1,10 @@
 import Stripe from "stripe";
 import { Price, Product } from "../supabase/supabase.types";
-import { prices, products } from "../../../migrations/schema";
+import { customers, prices, products, users } from "../../../migrations/schema";
+import { stripe } from "./index";
 import db from "../supabase/db";
+import { eq } from "drizzle-orm";
+
 export const upserProductRecord = async (product: Stripe.Product) => {
   const productData: Product = {
     id: product.id,
@@ -46,4 +49,60 @@ export const upsertPriceRecord = async (price: Stripe.Price) => {
     throw new Error(`Could not insert/update the price ${error}`);
   }
   console.log(`Price inserted/updated: ${price.id}`);
+};
+
+export const createOrRetrieveCustomer = async ({
+  email,
+  uuid,
+}: {
+  email: string;
+  uuid: string;
+}) => {
+  try {
+    const response = await db.query.customers.findFirst({
+      where: (c, { eq }) => eq(c.id, uuid),
+    });
+    if (!response) throw new Error();
+    return response.stripeCustomerId;
+  } catch (error) {
+    const customerData: { metadata: { supabaseUUID: string }; email?: string } =
+      {
+        metadata: {
+          supabaseUUID: uuid,
+        },
+      };
+    if (email) customerData.email = email;
+    try {
+      const customer = await stripe.customers.create(customerData);
+      await db
+        .insert(customers)
+        .values({ id: uuid, stripeCustomerId: customer.id });
+      console.log(`New customer created and inserted for ${uuid}.`);
+      return customer.id;
+    } catch (stripeError) {
+      throw new Error("Could not create Customer or find the customer");
+    }
+  }
+};
+
+export const copyBillingDetailsToCostumer = async (
+  uuid: string,
+  payment_method: Stripe.PaymentMethod
+) => {
+  const customer = payment_method.customer as string;
+  const { name, phone, address } = payment_method.billing_details;
+  if (!name || !phone || !address) return;
+  //@ts-ignore
+  await stripe.customers.update(customer, { name, phone, address });
+  try {
+    await db
+      .update(users)
+      .set({
+        billingAddress: { ...address },
+        paymentMethod: { ...payment_method[payment_method.type] },
+      })
+      .where(eq(users.id, uuid));
+  } catch (error) {
+    throw new Error("Couldnot copy customer billing details");
+  }
 };
