f53d33b86c0833094054887fd8ca9c31fab13d88
romuloromulo <neoromulo@gmail.com>
2024-02-17 16:32:18 -0300

    atualizando constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -16,97 +16,97 @@ export const USERS = [
   {
     nome: "Alice",
     mensagem:
-      "O Cypress foi um divisor de águas para nossa equipe. Com seus testes de ponta a ponta confiáveis, identificamos bugs no início, resultando em ciclos de desenvolvimento mais rápidos e uma colaboração aprimorada.",
+      "O Workspress foi um divisor de águas para nossa equipe. Com seus testes de ponta a ponta confiáveis, identificamos bugs no início, resultando em ciclos de desenvolvimento mais rápidos e uma colaboração aprimorada.",
   },
   {
     nome: "Mariana",
     mensagem:
-      "Eu costumava passar horas depurando problemas no frontend, mas o Cypress simplificou tudo. Agora, sou mais produtivo, e meus colegas podem confiar em nosso código graças ao Cypress.",
+      "Eu costumava passar horas depurando problemas no frontend, mas o Workspress simplificou tudo. Agora, sou mais produtivo, e meus colegas podem confiar em nosso código graças ao Workspress.",
   },
   {
     nome: "Amanda",
     mensagem:
-      "O Cypress transformou a maneira como trabalhamos. Nossas equipes de QA e desenvolvimento estão na mesma página, e nossa produtividade disparou. É uma ferramenta indispensável.",
+      "O Workspress transformou a maneira como trabalhamos. Nossas equipes de QA e desenvolvimento estão na mesma página, e nossa produtividade disparou. É uma ferramenta indispensável.",
   },
   {
     nome: "David",
     mensagem:
-      "Eu estava cético no início, mas o Cypress superou minhas expectativas. Nossos prazos de projeto melhoraram, e a colaboração entre as equipes é perfeita.",
+      "Eu estava cético no início, mas o Workspress superou minhas expectativas. Nossos prazos de projeto melhoraram, e a colaboração entre as equipes é perfeita.",
   },
   {
     nome: "Jorge",
     mensagem:
-      "O Cypress tornou a escrita e execução de testes muito fácil. A produtividade de nossa equipe nunca foi tão alta, e entregamos software mais confiável.",
+      "O Workspress tornou a escrita e execução de testes muito fácil. A produtividade de nossa equipe nunca foi tão alta, e entregamos software mais confiável.",
   },
   {
     nome: "Frank",
     mensagem:
-      "Graças ao Cypress, eliminamos gargalos nos testes. Nossos desenvolvedores e testadores colaboram sem esforço, resultando em lançamentos mais rápidos.",
+      "Graças ao Workspress, eliminamos gargalos nos testes. Nossos desenvolvedores e testadores colaboram sem esforço, resultando em lançamentos mais rápidos.",
   },
   {
     nome: "Gabriel",
     mensagem:
-      "O Cypress melhorou significativamente nosso processo de desenvolvimento. Agora temos mais tempo para inovação, e nossos produtos têm uma qualidade superior.",
+      "O Workspress melhorou significativamente nosso processo de desenvolvimento. Agora temos mais tempo para inovação, e nossos produtos têm uma qualidade superior.",
   },
   {
     nome: "Henrique",
     mensagem:
-      "A interface amigável do Cypress facilitou a contribuição de membros não técnicos de nossa equipe para os testes. Nosso fluxo de trabalho é muito mais eficiente agora.",
+      "A interface amigável do Workspress facilitou a contribuição de membros não técnicos de nossa equipe para os testes. Nosso fluxo de trabalho é muito mais eficiente agora.",
   },
   {
     nome: "Isabela",
     mensagem:
-      "A colaboração de nossa equipe melhorou imensamente com o Cypress. Identificamos problemas no início, resultando em menos atritos e implantações de recursos mais rápidas.",
+      "A colaboração de nossa equipe melhorou imensamente com o Workspress. Identificamos problemas no início, resultando em menos atritos e implantações de recursos mais rápidas.",
   },
   {
     nome: "João",
     mensagem:
-      "As capacidades robustas de teste do Cypress elevaram nossos padrões de desenvolvimento. Trabalhamos de maneira mais harmoniosa, e nossos lançamentos são mais confiáveis.",
+      "As capacidades robustas de teste do Workspress elevaram nossos padrões de desenvolvimento. Trabalhamos de maneira mais harmoniosa, e nossos lançamentos são mais confiáveis.",
   },
   {
     nome: "Katherine",
     mensagem:
-      "O Cypress é um salva-vidas para nossas equipes multifuncionais. Somos mais produtivos, e há um senso compartilhado de responsabilidade pela qualidade do produto.",
+      "O Workspress é um salva-vidas para nossas equipes multifuncionais. Somos mais produtivos, e há um senso compartilhado de responsabilidade pela qualidade do produto.",
   },
   {
     nome: "Liam",
     mensagem:
-      "O Cypress nos ajudou a manter altos padrões de qualidade. A colaboração de nossa equipe melhorou, resultando em ciclos de desenvolvimento mais rápidos.",
+      "O Workspress nos ajudou a manter altos padrões de qualidade. A colaboração de nossa equipe melhorou, resultando em ciclos de desenvolvimento mais rápidos.",
   },
   {
     nome: "Mia",
     mensagem:
-      "O Cypress é uma ferramenta poderosa que melhorou nossa produtividade e colaboração. Agora é parte integrante de nosso processo de desenvolvimento.",
+      "O Workspress é uma ferramenta poderosa que melhorou nossa produtividade e colaboração. Agora é parte integrante de nosso processo de desenvolvimento.",
   },
   {
     nome: "Nathan",
     mensagem:
-      "A interface amigável e os relatórios detalhados do Cypress tornaram os testes um passeio. A produtividade de nossa equipe está em alta.",
+      "A interface amigável e os relatórios detalhados do Workspress tornaram os testes um passeio. A produtividade de nossa equipe está em alta.",
   },
   {
     nome: "Olivia",
     mensagem:
-      "Vimos benefícios imediatos em termos de produtividade e colaboração após adotar o Cypress. É uma ferramenta essencial para nosso fluxo de trabalho de desenvolvimento.",
+      "Vimos benefícios imediatos em termos de produtividade e colaboração após adotar o Workspress. É uma ferramenta essencial para nosso fluxo de trabalho de desenvolvimento.",
   },
   {
     nome: "Paulo",
     mensagem:
-      "O Cypress otimizou nosso processo de teste e aproximou nossas equipes. Somos mais eficientes e entregamos resultados melhores.",
+      "O Workspress otimizou nosso processo de teste e aproximou nossas equipes. Somos mais eficientes e entregamos resultados melhores.",
   },
   {
     nome: "Quiteria",
     mensagem:
-      "O Cypress foi um divisor de águas para nós. Nossa produtividade e colaboração melhoraram significativamente, resultando em um software melhor.",
+      "O Workspress foi um divisor de águas para nós. Nossa produtividade e colaboração melhoraram significativamente, resultando em um software melhor.",
   },
   {
     nome: "Rafaela",
     mensagem:
-      "Graças ao Cypress, nosso processo de teste é agora uma parte integrante de nosso ciclo de desenvolvimento. Nossas equipes colaboram sem esforço.",
+      "Graças ao Workspress, nosso processo de teste é agora uma parte integrante de nosso ciclo de desenvolvimento. Nossas equipes colaboram sem esforço.",
   },
   {
     nome: "Samuel",
     mensagem:
-      "O Cypress é uma ferramenta fantástica que revolucionou nosso fluxo de trabalho. Nossa produtividade e colaboração atingiram novas alturas.",
+      "O Workspress é uma ferramenta fantástica que revolucionou nosso fluxo de trabalho. Nossa produtividade e colaboração atingiram novas alturas.",
   },
 ];
 
